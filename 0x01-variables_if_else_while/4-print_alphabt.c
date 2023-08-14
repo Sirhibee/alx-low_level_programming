89cb7b073df3316b12ee2da3ca95e5afbb372adb
Sirhibee <sirhibees@gmail.com>
2023-08-14 00:55:20 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the alphabet in lowercase
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+	while (num < 10)
+	{
+		printf("%d", num);
+		num += 1;
+	}
+printf("\n");
+return (0);
+}
