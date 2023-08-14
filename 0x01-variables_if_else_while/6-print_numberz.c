ea434073f5d0620b8a0478e962a0e6a68b9d590c
Sirhibee <sirhibees@gmail.com>
2023-08-14 13:51:25 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,22 +1,19 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
- * main - prints all the numbers of base 16 in lowercase.
- *
- * Return: Always 0.
- *
+ * main - Entry point
+ * Description: prints char below 10
+ * Return: Always 0 (success)
  */
 
 int main(void)
 {
-	int num;
-	char letter;
+	int a;
 
-	for (num = 0; num < 10; num++)
-		putchar((num % 10) + '0');
-
-	for (letter = 'a'; letter <= 'f'; letter++)
-		putchar(letter);
-
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a + '0');
+	}
 	putchar('\n');
 	return (0);
 }
