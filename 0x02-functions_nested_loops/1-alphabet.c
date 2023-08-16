256e8574fd982326c1f60175bc7b194a59e4f889
Sirhibee <sirhibees@gmail.com>
2023-08-16 00:15:04 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,8 @@
 #include "main.h"
-
 /**
  * main - function
  * Return: 0
  */
-
 int main(void)
 {
 	_putchar('_');
@@ -16,6 +14,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return (0);
-
+return (0);
 }
