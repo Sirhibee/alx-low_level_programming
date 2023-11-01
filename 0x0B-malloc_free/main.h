7f6e63d6ee7a85b2c94ab6e7d6d8646400d5a30b
Sirhibee <sirhibees@gmail.com>
2023-10-31 17:16:07 -0700

    101-strtow.c

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
-	
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 char *create_array(unsigned int size, char c);
 int _putchar(char c);
 char *_strdup(char *str);
